6ce206cb76a1bab4323e0b98d40584d397031ce2
Szymon Nowakowski <szymon.nowakowski2807@gmail.com>
2026-05-08 18:45:29 +0200

    Delete funtion body from header file

--- AddressBook.h
+++ AddressBook.h
@@ -9,16 +9,7 @@ class AddressBook {
     private:
         std::vector<Contact> contacts;
     public:
-        void displayMenu() {
-            std::cout << "===== ADDRESS BOOK =====" << std::endl;
-            std::cout << "1. Add contact" << std::endl;
-            std::cout << "2. Display all contacts" << std::endl;
-            std::cout << "3. Edit contact" << std::endl;
-            std::cout << "4. Delete contact" << std::endl;
-            std::cout << "5. Search contact" << std::endl;
-            std::cout << "0. Exit" << std::endl << std::endl;
-        }
-
+        void displayMenu();
 };
 
 #endif
