f7b7f419181fa1174277222b6f218e3ff8cc291c
Szymon Nowakowski <szymon.nowakowski2807@gmail.com>
2026-05-09 20:08:11 +0200

    Add menu display and executing user option choice

--- address_book.cpp
+++ address_book.cpp
@@ -1,17 +1,23 @@
 #include "AddressBook.h"
+#include "Menu.h"
 #include "Contact.h"
 #include <iostream>
 // #include <string>
 // #include <vector>
 // #include <regex>
-// #include <limits>
 // #include <fstream>
 // #include <nlohmann/json.hpp>
 // using json = nlohmann::ordered_json;
 
 int main() {
     AddressBook addressBook(addressBook.loadContacts());
-    addressBook.displayMenu();
+    Menu menu(addressBook);
+    bool isWorking= true;
+
+    while(isWorking) {
+        menu.displayMenu();
+        isWorking = menu.executeMenuOption();
+    }
 
     return 0;
 }
