6f7b606b6d4788c470ac90c4fe15600f30ed7421
Szymon Nowakowski <szymon.nowakowski2807@gmail.com>
2026-05-09 19:33:08 +0200

    Add getters and setters bodies

--- Contact.cpp
+++ Contact.cpp
@@ -9,3 +9,58 @@ Contact::Contact(int id, std::string firstName, std::string lastName, std::strin
     _street = street;
     _city = city;
 }
+int Contact::getID() {
+    return _id;
+}
+
+void Contact::setID(int newID) {
+    _id = newID;
+}
+
+std::string Contact::getFirstName() {
+    return _firstName;
+}
+
+void Contact::setFirstName(std::string newFirstName) {
+    _firstName = newFirstName;
+}
+
+std::string Contact::getLastName() {
+    return _lastName;
+}
+
+void Contact::setLastName(std::string newLastName) {
+    _lastName = newLastName;
+}
+
+std::string Contact::getPhoneNumber() {
+    return _phoneNumber;
+}
+
+void Contact::setPhoneNumber(std::string newPhoneNumber) {
+    _phoneNumber = newPhoneNumber;
+}
+
+std::string Contact::getEmail() {
+    return _email;
+}
+
+void Contact::setEmail(std::string newEmail) {
+    _email = newEmail;
+}
+
+std::string Contact::getStreet() {
+    return _street;
+}
+
+void Contact::setStreet(std::string newStreet) {
+    _street = newStreet;
+}
+
+std::string Contact::getCity() {
+    return _city;
+}
+
+void Contact::setCity(std::string newCity) {
+    _city = newCity;
+}
