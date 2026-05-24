019fdbf8433762b81ff3c0c97d9aad595e91c1e8
Szymon Nowakowski <szymon.nowakowski2807@gmail.com>
2026-05-24 15:59:58 +0200

    Fixed formatting

---
+++ Utils.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+
+namespace Utils {
+    std::string getUserInputLine();
+    bool confirmDeletion();
+};
+
+namespace NSString {
+    std::string toLower(std::string phrase);
+};
